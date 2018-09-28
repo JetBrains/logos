b3d9f0471b1fa5f305666c29b4bc7da3595563c6
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-09-28 14:26:49 +0300

    Better logging

--- scripts/generate-logos.js
+++ scripts/generate-logos.js
@@ -20,18 +20,17 @@ FileSet.create({
     const { basename, dirname, filename } = file;
     const copyPromise = file.copy(`${distDir}/${dirname}/${filename}`);
 
-    console.log('Converting file', filename)
     if (basename === dirname) {
+      console.log('Converting file into PNG set:', filename)
       return copyPromise.then(() => Promise.map(rules.keys(), (ruleId) => {
         return rules.get(ruleId)
           .transformToPNG(file)
-          .then(file => {
-            console.log('File converted', filename)
-            file.write(`${distDir}/${dirname}/${ruleId}`)
-          });
-      }));
+          .then(file => file.write(`${distDir}/${dirname}/${ruleId}`));
+      }))
+        .then(() => console.log('File converted:', filename));
     }
 
+    console.log('Copying file:', filename)
     return copyPromise;
   });
 });
