f1ca911f62db6719d9ccf81bdc64a9d5e66ee925
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2022-08-15 17:59:48 +0200

    feat: use "sharp" for image resizing when generating PNG
    Instead of obsolete "cherio" package

--- scripts/generate-logos.config.js
+++ scripts/generate-logos.config.js
@@ -1,22 +1,21 @@
 /**
  * Format:
- * - transform rule as string 'canvasWidth canvasHeight offsetX offsetY imageWidth'
  * - transform rule as object {@see TransformRule}
  */
 module.exports = {
-  'apple-touch-icon-57x57.png': '57 57 4',
-  'apple-touch-icon-60x60.png': '60 60 4',
-  'apple-touch-icon-72x72.png': '72 72 5',
-  'apple-touch-icon-76x76.png': '76 76 6',
-  'apple-touch-icon-114x114.png': '114 114 8',
-  'apple-touch-icon-120x120.png': '120 120 9',
-  'apple-touch-icon-144x144.png': '144 144 10',
-  'apple-touch-icon-152x152.png': '152 152 11',
-  'apple-touch-icon-180x180.png': '180 180 13',
-  'mstile-70x70.png': '128 128 16',
-  'mstile-144x144.png': '144 144',
-  'mstile-150x150.png': {width: 270, height: 270, offsetX: 72, offsetY: 50, imageWidth: 126},
-  'mstile-310x150.png': {width: 558, height: 270, offsetX: 216, offsetY: 50, imageWidth: 126},
-  'mstile-310x310.png': {width: 558, height: 558, offsetX: 150, offsetY: 128, imageWidth: 260},
-  'og-image-1200x630.png': {width: 1200, height: 630, offsetX: 335, offsetY: 50, imageWidth: 530},
+  'apple-touch-icon-57x57.png': {width: 57, height: 57, paddingH: 4},
+  'apple-touch-icon-60x60.png': {width: 60, height: 60, paddingH: 4},
+  'apple-touch-icon-72x72.png': {width: 72, height: 72, paddingH: 5},
+  'apple-touch-icon-76x76.png': {width: 76, height: 76, paddingH: 6},
+  'apple-touch-icon-114x114.png': {width: 114, height: 114, paddingH: 8},
+  'apple-touch-icon-120x120.png': {width: 120, height: 120, paddingH: 9},
+  'apple-touch-icon-144x144.png': {width: 144, height: 144, paddingH: 10},
+  'apple-touch-icon-152x152.png': {width: 152, height: 152, paddingH: 11},
+  'apple-touch-icon-180x180.png': {width: 180, height: 180, paddingH: 13},
+  'mstile-70x70.png': {width: 128, height: 128, paddingH: 16},
+  'mstile-144x144.png': {width: 144, height: 144, paddingH: 0},
+  'mstile-150x150.png': {width: 270, height: 270, paddingH: 72},
+  'mstile-310x150.png': {width: 558, height: 270, paddingH: 216},
+  'mstile-310x310.png': {width: 558, height: 558, paddingH: 150},
+  'og-image-1200x630.png': {width: 1200, height: 630, paddingH: 335}
 };
