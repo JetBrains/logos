5dbdb35cc67d12dc5b3de88fa6701ad48f158ca6
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-05-06 18:56:17 +0200

    DSGN-19133 drop generation of obsolete PNG files

--- scripts/generate-logos.config.js
+++ scripts/generate-logos.config.js
@@ -3,19 +3,5 @@
  * - transform rule as object {@see TransformRule}
  */
 module.exports = {
-  'apple-touch-icon-57x57.png': {width: 57, height: 57, paddingH: 4},
-  'apple-touch-icon-60x60.png': {width: 60, height: 60, paddingH: 4},
-  'apple-touch-icon-72x72.png': {width: 72, height: 72, paddingH: 5},
-  'apple-touch-icon-76x76.png': {width: 76, height: 76, paddingH: 6},
-  'apple-touch-icon-114x114.png': {width: 114, height: 114, paddingH: 8},
-  'apple-touch-icon-120x120.png': {width: 120, height: 120, paddingH: 9},
-  'apple-touch-icon-144x144.png': {width: 144, height: 144, paddingH: 10},
-  'apple-touch-icon-152x152.png': {width: 152, height: 152, paddingH: 11},
-  'apple-touch-icon-180x180.png': {width: 180, height: 180, paddingH: 13},
-  'mstile-70x70.png': {width: 128, height: 128, paddingH: 16},
-  'mstile-144x144.png': {width: 144, height: 144, paddingH: 0},
-  'mstile-150x150.png': {width: 270, height: 270, paddingH: 72},
-  'mstile-310x150.png': {width: 558, height: 270, paddingH: 216},
-  'mstile-310x310.png': {width: 558, height: 558, paddingH: 150},
-  'og-image-1200x630.png': {width: 1200, height: 630, paddingH: 335}
+  'apple-touch-icon-180x180.png': {width: 180, height: 180, paddingH: 13}
 };
